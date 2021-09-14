674c673216c7ce6a748d3d231f67c8b0c555bb76
Vaishnavi Pawar <vpvaishnavipawar@gmail.com>
2021-09-14 16:00:00 -0400

    Added backend files (PHP, MySQL config)

---
+++ navbar.php
@@ -0,0 +1,44 @@
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@600&display=swap');
+.nav-link{
+	margin-right: 15px;
+	color: #f8f9fa;
+	letter-spacing: 0.5px;
+	transition: 0.5s;
+}
+.navbar-brand{
+	color: #4C4B4B;
+	letter-spacing: 0.5px;
+  
+}
+h1{
+  color: rgb(175,125,0);
+}
+h2{
+	color: #4C4B4B;
+	letter-spacing: 0.5px;
+	font-family: raleway;
+}
+
+</style>
+
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <div class="con2">
+                <img src="img/logo.png" alt="Bank logo" text-align="left" width=100px  height="100px"></img>
+            </div>
+            
+      <a class="navbar-brand" href="index.php" style="font-family:sans-serif; font-weight:500" > <h1>The Sparks Foundation Bank</h1></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">HISTORY</a>
+              </li>
+          </div>
+       </nav>
